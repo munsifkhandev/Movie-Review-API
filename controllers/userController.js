283642c62bdbb185f941a751baa8fc38e4a8e5da
munsifkhandev <munsifkhandev@gmail.com>
2025-10-17 10:07:18 +0500

    Added a Register User Functionality

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User with this Email Already Exists..",
+      });
+    }
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+    return res.status(201).json({
+      success: true,
+      data: {
+        _id: savedUser._id,
+        name: savedUser.name,
+        email: savedUser.email,
+      },
+      message: "User Created Successfullyy..",
+    });
+  } catch (error) {
+    console.error(
+      "Server Error Occurred with Registering User..",
+      error.message
+    );
+    return res.status(500).json({
+      success: false,
+      message: "Error Creating User.",
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 connectDB();
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 4455;
+
+app.use("/api/v1/", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Helloo");
